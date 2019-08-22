49512a9cfc1696bf6f3aeb2a6c5c5cdd64f6a931
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2019-08-22 11:20:54 +0200

    Updated Demo code.

--- src/main/java/mask/registration/DemoFourpointsStandard.java
+++ src/main/java/mask/registration/DemoFourpointsStandard.java
@@ -4,14 +4,17 @@ import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.List;
 
+import mask.registration.alignment.RigidTransform;
 import mask.registration.displacement.Displacement;
 import mask.registration.displacement.DisplacementSummary;
 import mask.registration.displacement.SiteClass;
 import mask.registration.displacement.SiteSelection;
+import mask.registration.distortions.AffineTransform;
 import mask.registration.file.FileLoader;
 import mask.registration.firstorder.Alignments;
 import mask.registration.firstorder.Compensations;
 import mask.registration.firstorder.FirstOrderCorrection;
+import mask.registration.firstorder.FirstOrderResult;
 import mask.registration.firstorder.FirstOrderSetup;
 
 public class DemoFourpointsStandard {
@@ -24,29 +27,47 @@ public class DemoFourpointsStandard {
 	}
 
 	private void run() {
-		System.out.println(System.lineSeparator() + "--- DEMO: Standard 4-point alignment ----------");
+		System.out.println(System.lineSeparator() + "--- DEMO: standard 4-point alignment -----------------------");
 		
+		// STEP 1, load displacements from file (or any other source)
 		List<Displacement> displacements = new FileLoader().load(Paths.get("Demo-4Point.csv"));
 		
-		SiteSelection selection = SiteSelection
-					.forAlignment(d -> d.isOfType(SiteClass.ALIGN_MARK))
-					.forCalculation(d->true)
-					.build()
-					.remove(d->d.isOfType(SiteClass.INFO_ONLY));
 	
-		FirstOrderCorrection correction = new FirstOrderCorrection();
-		FirstOrderSetup setup = new FirstOrderSetup()
-										.withAlignment(Alignments.SELECTED)
-										.withCompensations(Compensations.SCALE, Compensations.ORTHO)
-										.withSiteSelection(selection);
+		// STEP 2, perform site selection 
+		SiteSelection selection = SiteSelection
+								.forAlignment(d -> d.isOfType(SiteClass.ALIGN_MARK))
+								.forCalculation(d->true)
+								.build()
+								.remove(d->d.isOfType(SiteClass.INFO_ONLY));
+	
+		// STEP 3, parametrize evaluation model 
+		FirstOrderSetup setup = FirstOrderSetup
+								.usingAlignment(Alignments.SELECTED)
+								.withCompensations(Compensations.SCALE, Compensations.ORTHO)
+								.withSiteSelection(selection);
+
+		// STEP 4, perform correction and calculate results
+		FirstOrderResult result = FirstOrderCorrection.using(displacements, setup);
+		Collection<Displacement> results = result.getDisplacements();
 		
-		Collection<Displacement> results = correction.apply(displacements, setup);
+		// STEP 5, print results
 		
+		// Now print results before correction
 		DisplacementSummary uncorrectedSummary = Displacement.summarize(displacements, selection.getCalculation());
-		System.out.println(System.lineSeparator()+ "--- unaligned --------------------------------" + uncorrectedSummary);
+		System.out.println(System.lineSeparator() + "--- unaligned ----------------------------------------------" + uncorrectedSummary);
 		
+		// after correction
 		DisplacementSummary correctedSummary = Displacement.summarize(results, selection.getCalculation());
-		System.out.println(System.lineSeparator()+ "--- corrected --------------------------------" + correctedSummary);
+		System.out.println(System.lineSeparator()+ "--- corrected ----------------------------------------------" + correctedSummary);
+		
+		// now also print residual first order and alignment
+		System.out.println("--- Residual Alignment and First Order ------------------------------------");
+		
+		RigidTransform correctedAlignment = result.getAlignment();
+		System.out.println(System.lineSeparator() + correctedAlignment);
+		
+		AffineTransform correctedFirstOrder = result.getFirstOrder();
+		System.out.println(System.lineSeparator() + correctedFirstOrder);
 		
 	}
 }
--- src/main/java/mask/registration/DemoMultipoint.java
+++ src/main/java/mask/registration/DemoMultipoint.java
@@ -4,12 +4,15 @@ import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.List;
 
+import mask.registration.alignment.RigidTransform;
 import mask.registration.displacement.Displacement;
 import mask.registration.displacement.DisplacementSummary;
 import mask.registration.displacement.SiteSelection;
+import mask.registration.distortions.AffineTransform;
 import mask.registration.file.FileLoader;
 import mask.registration.firstorder.Alignments;
 import mask.registration.firstorder.FirstOrderCorrection;
+import mask.registration.firstorder.FirstOrderResult;
 import mask.registration.firstorder.FirstOrderSetup;
 
 public class DemoMultipoint {
@@ -22,27 +25,46 @@ public class DemoMultipoint {
 	}
 
 	private void run() {
-		System.out.println(System.lineSeparator() + "--- DEMO: Multi point alignment ---------------");
+
+		System.out.println(System.lineSeparator() + "--- DEMO: Multipoint alignment	-----------------------------");
 		
+		// STEP 1, load displacements from file (or any other source)
 		List<Displacement> displacements = new FileLoader().load(Paths.get("Demo-4Point.csv"));
 		
-		SiteSelection selection = SiteSelection
-					.forAlignment(d->true)  // FIXME: There is still a unlucky conflict between site selection and alignment mode
-					.forCalculation(d->true)
-					.build();
 	
-		FirstOrderCorrection correction = new FirstOrderCorrection();
-		FirstOrderSetup setup = new FirstOrderSetup()
-										.withAlignment(Alignments.ALL)
-										.withSiteSelection(selection);
+		// STEP 2, perform site selection 
+		SiteSelection selection = SiteSelection
+								.forAlignment(d -> true)
+								.forCalculation(d->true)
+								.build();
+	
+		// STEP 3, parametrize evaluation model 
+		FirstOrderSetup setup = FirstOrderSetup
+								.usingAlignment(Alignments.ALL)
+								.withSiteSelection(selection);
+
+		// STEP 4, perform correction and calculate results
+		FirstOrderResult result = new FirstOrderCorrection().apply(displacements, setup);
+		Collection<Displacement> results = result.getDisplacements();
 		
-		Collection<Displacement> results = correction.apply(displacements, setup);
+		// STEP 5, print results
 		
+		// Now print results before correction
 		DisplacementSummary uncorrectedSummary = Displacement.summarize(displacements, selection.getCalculation());
-		System.out.println(System.lineSeparator()+ "--- unaligned --------------------------------" + uncorrectedSummary);
+		System.out.println(System.lineSeparator() + "--- unaligned ----------------------------------------------" + uncorrectedSummary);
 		
+		// after correction
 		DisplacementSummary correctedSummary = Displacement.summarize(results, selection.getCalculation());
-		System.out.println(System.lineSeparator()+ "--- multipoint corrected ---------------------" + correctedSummary);
+		System.out.println(System.lineSeparator()+ "--- corrected ----------------------------------------------" + correctedSummary);
+		
+		// now also print residual first order and alignment
+		System.out.println("--- Residual Alignment and First Order ------------------------------------");
+		
+		RigidTransform correctedAlignment = result.getAlignment();
+		System.out.println(System.lineSeparator() + correctedAlignment);
+		
+		AffineTransform correctedFirstOrder = result.getFirstOrder();
+		System.out.println(System.lineSeparator() + correctedFirstOrder);
 		
 	}
 }
