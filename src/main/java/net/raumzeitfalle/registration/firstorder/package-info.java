69e6cb06054b21429a9f21fdb552c58b42246286
Oliver Loeffler <oliver@raumzeitfalle.de>
2019-09-08 20:07:32 +0200

    Package infos added.

---
+++ src/main/java/net/raumzeitfalle/registration/firstorder/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * First order image placement correction and calculation similar to what is used in photomask manufacturing (rigid 3-parameter model for alignment and affine 6-parameter model for image registration evaluation and correction). Alignment and 6-parameter evaluation steps are configurable.
+ */
+package net.raumzeitfalle.registration.firstorder;
