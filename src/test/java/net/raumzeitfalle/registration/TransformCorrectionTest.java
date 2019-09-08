b21c29bd2feb1d00a26f798e4b87538613d50937
Oliver <oliver@raumzeitfalle.de>
2019-09-08 11:24:25 +0200

    Added skip test for TransformCorrection.

--- src/test/java/net/raumzeitfalle/registration/TransformCorrectionTest.java
+++ src/test/java/net/raumzeitfalle/registration/TransformCorrectionTest.java
@@ -1,6 +1,7 @@
 package net.raumzeitfalle.registration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -8,8 +9,10 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import net.raumzeitfalle.registration.alignment.RigidTransform;
 import net.raumzeitfalle.registration.displacement.Displacement;
+import net.raumzeitfalle.registration.distortions.AffineTransform;
+import net.raumzeitfalle.registration.distortions.AffineTransformBuilder;
+import net.raumzeitfalle.registration.distortions.SimpleAffineTransform;
 
 class TransformCorrectionTest {
 
@@ -17,7 +20,6 @@ class TransformCorrectionTest {
 	void apply() {
 		
 		Transform transform = new Transform() {
-			
 			@Override
 			public Displacement apply(Displacement t) {
 				return t.correctBy(10, -10);
@@ -39,6 +41,27 @@ class TransformCorrectionTest {
 			assertEquals( 990, d.getXd());
 			assertEquals(1010, d.getYd());
 		});
+	}
+	
+	@Test
+	void skip() {
+		
+		AffineTransform template = SimpleAffineTransform.with(0,0,0,0,0,0,0,0);
+		AffineTransform skipTransform = new AffineTransformBuilder(template).build();
+		
+		assertTrue(skipTransform.skip());
+		
+		TransformCorrection correctionFunction = new TransformCorrection();
+		
+		Displacement sourceElement = Displacement.at(1, 1, 1000, 1000);
+		Collection<Displacement> source = Arrays.asList(sourceElement);
+		
+		List<Displacement> result = (List<Displacement>) correctionFunction.apply(skipTransform, source);
+		
+		assertEquals(1, result.size());
+		assertEquals(sourceElement, result.get(0));
+	 	assertEquals(source, result);
+	 	
 	}
 
 }
