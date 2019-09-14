f46c7b78258a217df8ff4794234378d3038f4853
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2019-09-14 13:18:36 +0200

    Added test to handle case of data distributed along one straight line.

--- src/test/java/net/raumzeitfalle/registration/alignment/RigidTransformCalculationTest.java
+++ src/test/java/net/raumzeitfalle/registration/alignment/RigidTransformCalculationTest.java
@@ -240,5 +240,30 @@ class RigidTransformCalculationTest {
 		assertEquals(  0.0, result.getRotation(),     TOLERANCE);
 		
 	}
+	
+	@Test
+	void displacementsAlongHorizontalLine() {
+		
+		// 1ppm rotation	
+		List<Displacement> undisplaced = new ArrayList<>(5);
+		undisplaced.add(Displacement.at(0, 0,  20000, 70000,  20000-.06, 70000-.06));
+		undisplaced.add(Displacement.at(1, 1,  40000, 70000,  40000-.04, 70000-.04));
+		undisplaced.add(Displacement.at(2, 2,  60000, 70000,  60000-.02, 70000-.02));
+		undisplaced.add(Displacement.at(3, 3,  80000, 70000,  80000+.00, 70000+.00));
+		undisplaced.add(Displacement.at(4, 4, 100000, 70000, 100000+.02, 70000+.02));
+		undisplaced.add(Displacement.at(5, 5, 120000, 70000, 120000+.04, 70000+.04));
+		undisplaced.add(Displacement.at(6, 6, 140000, 70000, 140000+.06, 70000+.06));
+		
+		
+		RigidTransform result = funtionUnderTest.apply(undisplaced, d->true);
+		
+		assertNotNull(result);
+		assertTrue(result.getClass().equals(SimpleRigidTransform.class));
+		
+		assertEquals(  69.999 , result.getTranslationX()*1E3, 1E-3);
+		assertEquals( -79.999 , result.getTranslationY()*1E3, 1E-3);
+		assertEquals(    1E-6 , result.getRotation(),     TOLERANCE);
+		
+	}
 
 }
