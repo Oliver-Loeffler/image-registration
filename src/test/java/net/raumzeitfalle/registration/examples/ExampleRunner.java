77c14e78da47213d4414949680a78e5d70433cd8
Oliver <oliver@raumzeitfalle.de>
2019-09-15 12:25:01 +0200

    Added a runner for examples in test.

---
+++ src/test/java/net/raumzeitfalle/registration/examples/ExampleRunner.java
@@ -0,0 +1,33 @@
+package net.raumzeitfalle.registration.examples;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleRunner {
+
+	@Test
+	void runDemos() {
+		List<Runnable> examples = new ArrayList<>();
+		
+		examples.add(()->DemoFourpointsOnlyWithMissingMeas.main());
+		examples.add(()->DemoFourpointsScanner.main());
+		examples.add(()->DemoFourpointsStandard.main());
+		examples.add(()->DemoMultipoint.main());
+		examples.add(()->DemoMultipointMagnification.main());
+		examples.add(()->DemoMultipointOneDimensional.main());
+		examples.add(()->DemoMultipointResidual.main());
+		examples.add(()->DemoUnaligned.main());
+		
+		
+		examples.parallelStream().forEach(this::runDemo);
+	}
+	
+	private void runDemo(Runnable runnable) {
+		assertDoesNotThrow(()->runnable.run());
+	}
+
+}
