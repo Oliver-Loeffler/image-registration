82581de8a4bb8cffe03a80bfd17506b61fd5188f
Oliver <oliver@raumzeitfalle.de>
2019-09-18 06:43:43 +0200

    Added further tests for 1D distributed data with partially X or Y readings.

--- src/test/java/net/raumzeitfalle/registration/distortions/OneDimensionalAffineTransformCalculationTest.java
+++ src/test/java/net/raumzeitfalle/registration/distortions/OneDimensionalAffineTransformCalculationTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import net.raumzeitfalle.registration.displacement.Displacement;
@@ -56,6 +57,7 @@ class OneDimensionalAffineTransformCalculationTest {
 		assertEquals(  -1.000, result.getOrtho()*1E6,         1E-6);
 	}
 	
+	@Disabled("Not yet implemented")
 	@Test
 	void displacementsAlongVerticalLine_oneDimensional_y() {
 		// 1ppm rotation
@@ -90,6 +92,7 @@ class OneDimensionalAffineTransformCalculationTest {
 		assertEquals(  -1.000, result.getOrtho()*1E6,         1E-6);
 	}
 	
+	@Disabled("Not yet implemented")
 	@Test
 	void displacementsAlongVerticalLine_oneDimensional_x() {
 		// 1ppm rotation
@@ -138,6 +141,76 @@ class OneDimensionalAffineTransformCalculationTest {
 		undisplaced.add(Displacement.at(4, 4, 100000, 70000, 100000+.02+tx, 70000+.02+ty));
 		undisplaced.add(Displacement.at(5, 5, 120000, 70000, 120000+.04+tx, 70000+.04+ty));
 		undisplaced.add(Displacement.at(6, 6, 140000, 70000, 140000+.06+tx, 70000+.06+ty));
+			
+		AffineTransform result = funtionUnderTest.apply(undisplaced, d->true);
+		
+		assertTrue(result.getClass().equals(SimpleAffineTransform.class));
+		
+		assertEquals(  tx, result.getTranslationX(),  TOLERANCE);
+		assertEquals(  ty, result.getTranslationY(),  TOLERANCE);
+		
+		assertEquals( 80000.0, result.getCenterX(),    TOLERANCE);
+		assertEquals( 70000.0, result.getCenterY(),    TOLERANCE);
+		
+		assertEquals(  1.000, result.getScaleX()*1E6,        1E-6);
+		assertEquals(  0.0,   result.getScaleY()*1E6,        1E-6);
+		assertEquals(  0.500, result.getMagnification()*1E6, 1E-6);
+		
+		assertEquals( -1.000, result.getOrthoX()*1E6,        1E-6);
+		assertEquals(  0.000, result.getOrthoY()*1E6,        TOLERANCE);
+		assertEquals(  1.000, result.getOrtho()*1E6,         1E-6);
+	}
+	
+	@Disabled("Not yet implemented")
+	@Test
+	void displacementsAlongHorizontalLine_oneDimensional_x() {
+		// 1ppm rotation
+
+		double tx = 0.1;
+		double ty = -.01;
+		List<Displacement> undisplaced = new ArrayList<>(5);
+		undisplaced.add(Displacement.at(0, 0,  20000, 70000,  20000-.06+tx, Double.NaN));
+		undisplaced.add(Displacement.at(1, 1,  40000, 70000,  40000-.04+tx, Double.NaN));
+		undisplaced.add(Displacement.at(2, 2,  60000, 70000,  60000-.02+tx, Double.NaN));
+		undisplaced.add(Displacement.at(3, 3,  80000, 70000,  80000+.00+tx, Double.NaN));
+		undisplaced.add(Displacement.at(4, 4, 100000, 70000, 100000+.02+tx, Double.NaN));
+		undisplaced.add(Displacement.at(5, 5, 120000, 70000, 120000+.04+tx, Double.NaN));
+		undisplaced.add(Displacement.at(6, 6, 140000, 70000, 140000+.06+tx, Double.NaN));
+			
+		AffineTransform result = funtionUnderTest.apply(undisplaced, d->true);
+		
+		assertTrue(result.getClass().equals(SimpleAffineTransform.class));
+		
+		assertEquals(  tx, result.getTranslationX(),  TOLERANCE);
+		assertEquals(  ty, result.getTranslationY(),  TOLERANCE);
+		
+		assertEquals( 80000.0, result.getCenterX(),    TOLERANCE);
+		assertEquals( 70000.0, result.getCenterY(),    TOLERANCE);
+		
+		assertEquals(  1.000, result.getScaleX()*1E6,        1E-6);
+		assertEquals(  0.0,   result.getScaleY()*1E6,        1E-6);
+		assertEquals(  0.500, result.getMagnification()*1E6, 1E-6);
+		
+		assertEquals( -1.000, result.getOrthoX()*1E6,        1E-6);
+		assertEquals(  0.000, result.getOrthoY()*1E6,        TOLERANCE);
+		assertEquals(  1.000, result.getOrtho()*1E6,         1E-6);
+	}
+	
+	@Disabled("Not yet implemented")
+	@Test
+	void displacementsAlongHorizontalLine_oneDimensional_y() {
+		// 1ppm rotation
+
+		double tx = 0.1;
+		double ty = -.01;
+		List<Displacement> undisplaced = new ArrayList<>(5);
+		undisplaced.add(Displacement.at(0, 0,  20000, 70000, Double.NaN, 70000-.06+ty));
+		undisplaced.add(Displacement.at(1, 1,  40000, 70000, Double.NaN, 70000-.04+ty));
+		undisplaced.add(Displacement.at(2, 2,  60000, 70000, Double.NaN, 70000-.02+ty));
+		undisplaced.add(Displacement.at(3, 3,  80000, 70000, Double.NaN, 70000+.00+ty));
+		undisplaced.add(Displacement.at(4, 4, 100000, 70000, Double.NaN, 70000+.02+ty));
+		undisplaced.add(Displacement.at(5, 5, 120000, 70000, Double.NaN, 70000+.04+ty));
+		undisplaced.add(Displacement.at(6, 6, 140000, 70000, Double.NaN, 70000+.06+ty));
 			
 		AffineTransform result = funtionUnderTest.apply(undisplaced, d->true);
 		
