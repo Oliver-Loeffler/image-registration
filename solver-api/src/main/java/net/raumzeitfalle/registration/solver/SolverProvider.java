6b291eb9c612b1ba6db367c409b8c9c49af23851
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2020-08-01 19:09:34 +0200

    Moved solver code into solver-api project.
    Created inter-project dependencies.

--- solver-api/src/main/java/net/raumzeitfalle/registration/solver/SolverProvider.java
+++ solver-api/src/main/java/net/raumzeitfalle/registration/solver/SolverProvider.java
@@ -1,13 +1,11 @@
 package net.raumzeitfalle.registration.solver;
 
-import net.raumzeitfalle.registration.jama.JamaSolver;
-
 public class SolverProvider {
 	
 	public Solver getSolver() {
 	    // return new EjmlSolver();
 	    // return new La4jSolver();
-		return new JamaSolver();
+		return null; //return new JamaSolver();
 		// return new ApacheMathCommonsSolver();
 	}
 	
