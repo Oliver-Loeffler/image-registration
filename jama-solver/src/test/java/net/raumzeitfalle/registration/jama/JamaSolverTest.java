e7c87930a68bdc8db177bdba096ed92f2cbc0cfb
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2025-01-28 19:12:22 +0100

    Added missing imports.

--- jama-solver/src/test/java/net/raumzeitfalle/registration/jama/JamaSolverTest.java
+++ jama-solver/src/test/java/net/raumzeitfalle/registration/jama/JamaSolverTest.java
@@ -19,12 +19,11 @@
  */
 package net.raumzeitfalle.registration.jama;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
 import net.raumzeitfalle.registration.solver.Solution;
 import net.raumzeitfalle.registration.solver.spi.SolverAdapter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class JamaSolverTest {
 
@@ -34,7 +33,6 @@ class JamaSolverTest {
 	
 	@Test
 	void test() {
-
 		double[][] design = { 
 				{ -75000.0, 0.0, 0.0, -70000.0, 1.0, 0.0 }, 
 				{ 0.0, -70000.0, 75000.0, 0.0, 0.0, 1.0 },
@@ -47,7 +45,6 @@ class JamaSolverTest {
 
 		double[] differences = { -0.075, 0.140, -0.075, -0.140, 0.075, -0.140, 0.075, 0.140 };
 
-		
 		Solution solution = classUnderTest.apply(() -> design, () -> differences);
 
 		double[] result = { 1.0E-6, -2.0E-6, 0.0, 0.0, 0.0, 0.0 };
@@ -62,7 +59,5 @@ class JamaSolverTest {
 
 				() -> assertEquals(result[4], solution.get(4), TOLERANCE, "trans x"),
 				() -> assertEquals(result[5], solution.get(5), TOLERANCE, "trans y"));
-
 	}
-
 }
